97eb1b4fa17860bd09fdf17508b45a684b8ab748
Edinei J. Bauer <galera.org@gmail.com>
2019-07-24 21:59:43 -0300

    evita duplicação js

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,58 +1,61 @@
 $(function () {
-    let equipe = dbLocal.exeRead("equipe");
-    let template = dbLocal.exeRead("__template", 1);
+    if (typeof dup === 'undefined') {
+        var dup = 1;
+        let equipe = dbLocal.exeRead("equipe");
+        let template = dbLocal.exeRead("__template", 1);
 
-    let voce1 = {
-        'nome': 'Vaga em aberto',
-        'atuacao': 'Desenvolvedor React',
-        'imagem': HOME + 'public/assets/img/user.png',
-        'descricao': '',
-    };
-    let voce2 = {
-        'nome': 'Vaga em aberto',
-        'atuacao': 'Desenvolvedor Moodle',
-        'imagem': HOME + 'public/assets/img/user.png',
-        'descricao': '',
-    };
+        let voce1 = {
+            'nome': 'Vaga em aberto',
+            'atuacao': 'Desenvolvedor React',
+            'imagem': HOME + 'public/assets/img/user.png',
+            'descricao': '',
+        };
+        let voce2 = {
+            'nome': 'Vaga em aberto',
+            'atuacao': 'Desenvolvedor Moodle',
+            'imagem': HOME + 'public/assets/img/user.png',
+            'descricao': '',
+        };
 
-    Promise.all([equipe, template]).then(d => {
-        $.each(d[0], function (i, e) {
-            e.imagem = e.imagem[0].url;
-            $("#equipe").append(Mustache.render(d[1].time, e));
+        Promise.all([equipe, template]).then(d => {
+            $.each(d[0], function (i, e) {
+                e.imagem = e.imagem[0].url;
+                $("#equipe").append(Mustache.render(d[1].time, e));
+            });
+            $("#equipe").append(Mustache.render(d[1].time, voce1));
+            $("#equipe").append(Mustache.render(d[1].time, voce2));
+
+            if(!navigator.onLine) {
+                $("img").each(function (i, img) {
+                    let pat = new RegExp("\/uploads\/", "i");
+                    if(pat.test($(img).attr("src"))) {
+                        $(img).attr("src", HOME + "public/assets/img/user.png");
+                    };
+                })
+            }
         });
-        $("#equipe").append(Mustache.render(d[1].time, voce1));
-        $("#equipe").append(Mustache.render(d[1].time, voce2));
 
-        if(!navigator.onLine) {
-            $("img").each(function (i, img) {
-                let pat = new RegExp("\/uploads\/", "i");
-                if(pat.test($(img).attr("src"))) {
-                    $(img).attr("src", HOME + "public/assets/img/user.png");
-                };
-            })
-        }
-    });
+        /**
+         * Animação
+         * */
+        $(window).scroll(function (event) {
+            var st = $(this).scrollTop();
+            if (st > 400)
+                $(".ballon").css("transform", "translate(180px, 150px)");
+        });
 
-    /**
-     * Animação
-     * */
-    $(window).scroll(function (event) {
-        var st = $(this).scrollTop();
-        if (st > 400)
-            $(".ballon").css("transform", "translate(180px, 150px)");
-    });
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
 
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
             });
         });
-    });
 
-    console.log("Olá DEV, estamos contratando!");
+        console.log("Olá DEV, estamos contratando!");
+    }
 });
 
 $(function() {
